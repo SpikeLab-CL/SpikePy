9fdd8a889959e72815a2ae331399dd1ed8696fa3
cd <cdagnino@spikelab.xyz>
2018-06-08 17:13:22 -0400

    Implements function to export all relevant info to flask

--- SpikePy/__init__.py
+++ SpikePy/__init__.py
@@ -1,7 +1,8 @@
 #################
 #
-# March, 2018
+# March-June 2018
 #
 ################
 
 from .h2o_interface.main import *
+from .h2o_interface.utils import *
---
+++ SpikePy/h2o_interface/utils.py
@@ -0,0 +1,18 @@
+import dill
+
+def export_flask_dict(limedf, dfh2o, location=""):
+	"""
+	Takes a LimeDF object and the h2o dataframe with training or test data
+	and exports the flask dictionary necessary for using flask with the model
+	"""
+	flask_d = {'encodings' : limedf.label_encodings,
+			'column_types': dfh2o[limedf.x_vars].types,
+			'column_names': limedf.x_vars}
+
+	with open(location + "dict_for_flask.dill", 'wb') as f:
+		dill.dump(flask_d, f)
+		print("Dictionary saved at ", location + "dict_for_flask.dill")
+
+
+
+
